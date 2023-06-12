efed9a5b38db585b97c8e1a433175609e670b509
Andrei Gubani <116263639+TheOriginalAn3@users.noreply.github.com>
2023-06-12 15:35:39 +0200

    sss

--- GroupBy.sql
+++ GroupBy.sql
@@ -5,3 +5,9 @@
 #SELECT dauer,count(*) from Umfrage GROUP BY dauer HAVING count(*) = 1;
 #SELECT geschlecht,dauer, sum(dauer), avg(dauer) from Umfrage GROUP BY geschlecht;
 #SELECT sport, sum(dauer), avg(dauer) from Umfrage GROUP BY Sport HAVING count(sport) = 2;
+
+#AB. No.5
+#SELECT emp.deptno, dname, COUNT(*) from emp, dept WHERE emp.deptno = dept.deptno GROUP BY emp.deptno; 
+SELECT hiredate, emp.deptno, dept.dname, count(*) from emp, dept WHERE emp.deptno = dept.deptno AND hiredate LIKE "2001%" GROUP BY dept.dname;
+
+
